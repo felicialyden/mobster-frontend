410d5dd7fb5b9efc439f0bb4c60a1ac2159c53de
sgayathrii <sgayathrii@gmail.com>
2023-11-02 16:28:28 +0100

    Removed headers from fetch

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,6 @@ const fetchMobs = async () => {
   try {
     const data = await fetch(
       'https://mobster-backend-production-094a.up.railway.app/mobs',
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json',
-        },
-      }
     );
     console.log(data);
     const json = await data.json();
