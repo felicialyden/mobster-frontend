f842f49fdc310a626c1e279d7f775552bd45b09e
sgayathrii <sgayathrii@gmail.com>
2023-11-02 16:16:56 +0100

    Another improvisation fetch

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,24 @@
 import './style.css';
 
 const fetchMobs = async () => {
-  const data = await fetch(
-    'https://mobster-backend-production-094a.up.railway.app/',
-    {
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-      },
-    }
-  );
-  const json = await data.json();
-  console.log(json);
+  try {
+    const data = await fetch(
+      'https://mobster-backend-production-094a.up.railway.app/',
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+      }
+    );
+    console.log(data);
+    const json = await data.json();
+    console.log(json);
+  } catch (error) {
+    console.log("dsfsdgdsgfggg");
+    console.log(error);
+  }
+  
 };
 
 fetchMobs();
