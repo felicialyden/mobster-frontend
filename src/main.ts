93dff76a01704d8c2e7ed2db1511bf5d9c58d913
Felicia <felicia.lyden@appliedtechnology.se>
2023-11-02 10:37:37 +0100

    Add h1

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,8 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
+import ghpages from 'gh-pages'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+ghpages.publish('dist', function(err) { console.log(err)});
+
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
